01da76d7f186ed4f4f1f063d9344bed346edb694
Ashutosh <4ashutosh98@gmail.com>
2025-01-25 15:36:12 -0500

    Trained the model and created an application

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# lstm-hamlet-app
+# LSTM Hamlet Application
+
+This app contains a simple LSTM model that predicts the next word in a sequence of words.
+The model is trained on the Hamlet by William Shakespeare.
+This application is a simple demonstration of how to use LSTM models for text prediction.
+Libraries used: numpy, pandas, tensorflow, keras, nltk.
+A simple GUI is created using Streamlit for demonstration the application.
+
+The application is hosted on streamlit.io and can be accessed on the following link: [LSTM Hamlet Application](https://something.con)
---
+++ app.py
@@ -0,0 +1,35 @@
+import tokenize
+import streamlit as st
+import numpy as np
+import pickle
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+# load the LSTM model
+model = load_model('next_word_lstm.h5')
+
+# Load the tokenizer
+with open('tokenizer.pickle', 'rb') as handle:
+    tokenizer = pickle.load(handle)
+
+# Function to predict the next word
+def predict_next_word(model, tokenizer, text, max_sequence_len):
+    token_list = tokenizer.texts_to_sequences([text])[0]
+    if len(token_list) >= max_sequence_len:
+        token_list = token_list[-(max_sequence_len - 1):] # Ensure the sequence length matches max_sequence
+    token_list = pad_sequences([token_list], maxlen=max_sequence_len, padding='pre')
+    prediction = model.predict(token_list, verbose=0)
+    predicted_word_index = np.argmax(prediction, axis=1)
+    for word, index in tokenizer.word_index.items():
+        if index == predicted_word_index:
+            return word
+    return None
+
+# Streamlit app
+st.title("Next Word Prediction with LSTM and early stopping")
+input_text = st.text_input("Enter the sequence of words", "")
+
+if st.button("Predict Next Word"):
+    max_sequence_len = model.input_shape[1] + 1 # Retrive the max sequence length from the model
+    next_word = predict_next_word(model=model, tokenizer= tokenizer, text = input_text, max_sequence_len=max_sequence_len)
+    st.write(f"Next word: {next_word}")
